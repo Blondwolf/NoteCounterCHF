68dccc722c692e8d5d92b4fe29ac2cb13b646ae4
kevin.vulliemi <kevin.vulliemin@he-arc.ch>
2018-02-27 14:25:35 +0100

    Created bat launcher that auto-install virtual environnement (venv) with requirements. Base python file with hello world

---
+++ src/note_analyser.py
@@ -0,0 +1,17 @@
+__author__ = 'Lovis Thomas, Vulliemin Kevin'
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+
+"""
+ * Cours :              Traitement d'images
+ * Projet :     	    Compteur de billet Suisse / NoteCounterCHF
+ * Git : 				https://github.com/Blondwolf/NoteCounterCHF
+ * Description :        Faire du traitement sur une image contenant des billets de banque suisse afin de les classifier et en extraire le montant total.
+ * Suite à donner :    	Utilisation de la Webcam à la place des images.
+ * Auteurs :            Lovis Thomas & Vulliemin Kevin
+ * Date création :      27.02.2018
+ * Professeur :         Tièche François
+"""
+
+if __name__ == '__main__':
+    print("Hello noteCounter")
