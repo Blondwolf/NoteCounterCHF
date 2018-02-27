6706e6c0ac3a57962cb81223289f4f24e6dcfac2
kevin.vulliemi <kevin.vulliemin@he-arc.ch>
2018-02-27 18:35:18 +0100

    Began color identification

--- src/note_analyser.py
+++ src/note_analyser.py
@@ -13,5 +13,50 @@ __author__ = 'Lovis Thomas, Vulliemin Kevin'
  * Professeur :         Tièche François
 """
 
-if __name__ == '__main__':
+import sys
+import cv2
+import numpy as np
+
+
+def main():
     print("Hello noteCounter")
+
+    print(cv2.__version__)
+    img = cv2.imread('img/20f.jpg')
+
+    cv2.imshow("Image Originale", img)
+    cv2.waitKey(0)
+
+    colorOutput = colorDetection(img)
+
+    cv2.imshow("Detection de couleur", colorOutput)
+    cv2.waitKey(0)
+
+#https://www.pyimagesearch.com/2014/08/04/opencv-python-color-detection/
+def colorDetection(image):
+    # define the list of color range TODO choose wisely
+    colorBoundaries = [
+        ([17, 15, 100], [50, 56, 200]),     #Blue
+        ([86, 31, 4], [220, 88, 50]),       #Red
+        ([25, 146, 190], [62, 174, 250]),
+        ([103, 86, 65], [145, 133, 128])
+    ]
+
+    # loop over the boundaries
+    for (lower, upper) in colorBoundaries:
+        # create NumPy arrays from the boundaries
+        lower = np.array(lower, dtype="uint8")
+        upper = np.array(upper, dtype="uint8")
+
+        # find the colors within the specified boundaries and apply
+        # the mask
+        mask = cv2.inRange(image, lower, upper)
+        output = cv2.bitwise_and(image, image, mask=mask)
+
+        # show the images
+        cv2.imshow("images", np.hstack([image, output]))
+        cv2.waitKey(0)
+
+
+if __name__ == "__main__":
+    main()
