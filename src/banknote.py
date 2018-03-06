58da53ebb6667526244020e9e6fad2eda08e26e5
kevin.vulliemi <kevin.vulliemin@he-arc.ch>
2018-03-06 22:21:58 +0100

    V1 completed: -Simple color detection -Externalized analyse method -Program structurized

--- src/banknote.py
+++ src/banknote.py
@@ -1,8 +1,21 @@
 # -*- coding: utf-8 -*-
 
-
 class BankNote(object):
     """Represent a note with a value"""
 
     def __init__(self, value):
         self.value = value
+
+
+note_10 = BankNote(10)
+note_20 = BankNote(20)
+note_50 = BankNote(50)
+
+note_colors = {
+    "Yellow" : note_10,
+    "Red" : note_20,
+    "Green" : note_50
+}
+
+
+
--- src/note_analyser.py
+++ src/note_analyser.py
@@ -13,60 +13,38 @@ __author__ = 'Lovis Thomas, Vulliemin Kevin'
  * Professeur :         Tièche François
 """
 
-import sys
 import cv2
-import numpy as np
-from banknote import BankNote
+from image_reader import ImageReader
 
+img_reader = ImageReader()
+image_readed = 0
 
-# https://www.pyimagesearch.com/2014/08/04/opencv-python-color-detection/
-def color_detection(image):
-    # define the list of color range (bgr) TODO choose wisely
-    color_boundaries = [
-        ([50, 119, 200],[82, 147, 255]),    # YELLOW
-        ([37, 151, 199],[181, 213, 235]),   # YELLOW FULL
-        ([17, 15, 0], [50, 56, 255]),       # RED
-        # ([0, 0, 0], [100, 100, 255]),     # RED FULL
-        ([86, 31, 4], [220, 88, 50]),
-        ([103, 86, 65], [145, 133, 128])
-    ]
+def read_image(path):
+    global image_readed
 
-    # loop over the boundaries
-    for (lower, upper) in color_boundaries:
-        # create NumPy arrays from the boundaries
-        lower = np.array(lower, dtype="uint8")
-        upper = np.array(upper, dtype="uint8")
+    value, image = img_reader.read_image(path)
+    print("-> The image show a sum of " + str(value) + " !")
 
-        # find the colors within the specified boundaries and apply
-        # the mask
-        mask = cv2.inRange(image, lower, upper)
-        output = cv2.bitwise_and(image, image, mask=mask)
+    image = cv2.resize(image, (265, 470))
+    cv2.imshow(str(image_readed), image)
 
-        # Count no-black points
-        gray_image = cv2.cvtColor(output, cv2.COLOR_BGR2GRAY)
-        print("Non black point count :" + str(cv2.countNonZero(gray_image)))
-
-        # What is that
-        #kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
-        #cv2.erode(mask, kernel, mask)
-
-        # show the images
-        cv2.imshow("images", np.hstack([image, output]))
-        cv2.waitKey(0)
+    image_readed = image_readed + 1
 
 
 def main():
-    print("Hello noteCounter")
+    print("Starting NoteCounter")
+    print("Python version : "+cv2.__version__)
+    print("--------------------")
 
-    print(cv2.__version__)
-    img = cv2.imread('img/10f.jpg')
+    read_image('img/10f.jpg')
+    read_image('img/10b.jpg')
+    read_image('img/20f.jpg')
+    read_image('img/20b.jpg')
+    read_image('img/50f.jpg')
+    read_image('img/50b.jpg')
 
-    cv2.imshow("Image Originale", img)
-    cv2.waitKey(0)
+    #img_reader.show_images()
 
-    color_output = color_detection(img)
-
-    cv2.imshow("Detection de couleur", color_output)
     cv2.waitKey(0)
 
 
